21cad424c0c7d75bd3a74191938dd6d2bbe19221
joao_broering <joao_broering@estudante.sesisenai.org.br>
2023-03-22 12:17:45 -0300

    coloquei o textview

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,14 +3,17 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
+// 11
 public class MainActivity extends AppCompatActivity {
-
+    EditText n1, n2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        n1 = findViewById(R.id.n1);
 
 
 
