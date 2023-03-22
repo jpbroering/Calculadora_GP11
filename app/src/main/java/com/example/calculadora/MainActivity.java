b50395f5682423fbe80dd99cbc4ea1ad53c797d0
joao_broering <joao_broering@estudante.sesisenai.org.br>
2023-03-22 12:23:27 -0300

    coloquei o soma

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,19 +3,26 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 // 11
 public class MainActivity extends AppCompatActivity {
     EditText n1, n2;
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         n1 = findViewById(R.id.n1);
-
-
-
+        n2 = findViewById(R.id.n2);
+        result = findViewById(R.id.result);
+    }
+    public void soma(View v){
+        int nu1 = Integer.parseInt(n1.getText().toString());
+        int nu2 = Integer.parseInt(n2.getText().toString());
+        int tl = nu1+nu2;
+        result.setText(tl);
     }
 }
