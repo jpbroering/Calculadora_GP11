aaba9f1b479e4e4e983918adc8605826956e9de5
joao_broering <joao_broering@estudante.sesisenai.org.br>
2023-03-23 08:34:12 -0300

    terminei a calc

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 // 11
 public class MainActivity extends AppCompatActivity {
     EditText n1, n2;
     TextView result;
+    int nu1, nu2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,27 +22,40 @@ public class MainActivity extends AppCompatActivity {
         result = findViewById(R.id.result);
     }
     public void soma(View v){
-    int nu1 = Integer.parseInt(n1.getText().toString());
-    int nu2 = Integer.parseInt(n2.getText().toString());
-    int tl = nu1+nu2;
-    result.setText(tl+"");
+        num();
+        int tl = nu1+nu2;
+        result.setText(tl+"");
     }
     public void subtrair(View v){
-        int nu1 = Integer.parseInt(n1.getText().toString());
-        int nu2 = Integer.parseInt(n2.getText().toString());
+        num();
         int tl = nu1-nu2;
         result.setText(tl+"");
     }
     public void dividir(View v){
-        int nu1 = Integer.parseInt(n1.getText().toString());
-        int nu2 = Integer.parseInt(n2.getText().toString());
-        int tl = nu1/nu2;
-        result.setText(tl+"");
+        num();
+        if(nu1 == 0 || nu2 == 0){
+            result.setText("não da para dividir por 0");
+        }
+        else {
+            int tl = nu1 / nu2;
+            result.setText(tl + "");
+        }
     }
     public void multiplicar(View v){
-        int nu1 = Integer.parseInt(n1.getText().toString());
-        int nu2 = Integer.parseInt(n2.getText().toString());
+        num();
         int tl = nu1*nu2;
         result.setText(tl+"");
+    }
+    public void num(){
+        String snu1 = n1.getText().toString();
+        String snu2 = n2.getText().toString();
+        if (snu1.equals("")){
+            snu1 = "1";
+        }
+        if (snu2.equals("")){
+            snu2 = "1";
+        }
+        nu1 = Integer.parseInt(snu1);
+        nu2 = Integer.parseInt(snu2);
     }
 }
